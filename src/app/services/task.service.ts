9135c824d62a72d179d6d676f665314cca85c7df
Andy Vo <andyvo190998@gmail.com>
2024-04-15 20:46:17 +0200

    feat: first http get request

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
+  private apiUrl = "http://localhost:5001/tasks"
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
