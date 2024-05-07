51a60ffe4551a6878486492946f71b8978b1d6a7
Andy Vo <andyvo190998@gmail.com>
2024-05-07 18:18:38 +0200

    feat: create form for add new task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component'
 import { TasksComponent } from './components/tasks/tasks.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,7 +11,6 @@ import { TasksComponent } from './components/tasks/tasks.component';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
