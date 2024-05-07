5ee23f0010646c4a741fe512314d6934fcf7d9dc
Andy Vo <andyvo190998@gmail.com>
2024-05-07 18:59:33 +0200

    feat: toggle add new task form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
@@ -14,8 +17,12 @@ export class AddTaskComponent {
   text: string = '';
   day: string = '';
   reminder: boolean = false
+  onOpenForm: boolean = false
+  subscription: Subscription | undefined;
 
-  constructor() {}
+  constructor( private uiService: UiService ) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.onOpenForm = value)
+  }
 
   ngOnInit(): void {}
   onSubmit() {
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
-
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -10,11 +11,16 @@ import { ButtonComponent } from '../button/button.component';
 })
 export class HeaderComponent {
   title: string = 'Task Tracker';
-  constructor() {}
+  showAddTask: boolean = false;
+  subscription: Subscription | undefined;
+
+  constructor( private uiService: UiService ) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log("toggle")
+    this.uiService.toggleAddTask()
   }
 }
